203e3f18bd24d864d6d4aef5a2f0772125de46f7
PGTech-Services <pgtech.repairs@gmail.com>
2025-08-11 20:40:08 +1000

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-function toggleMenu(){
-  const hamburger = document.querySelector('.hamburger');
-  const menu = document.querySelector('.menu');
-  if (hamburger) hamburger.classList.toggle('active');
-  if (menu) menu.classList.toggle('active');
-}

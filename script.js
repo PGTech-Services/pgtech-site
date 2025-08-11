ff47089308a0bd1d7a33247317d120bcfe20d52e
PGTech-Services <pgtech.repairs@gmail.com>
2025-08-11 20:34:09 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-function toggleMenu() {
+function toggleMenu(){
   const hamburger = document.querySelector('.hamburger');
   const menu = document.querySelector('.menu');
-  hamburger.classList.toggle('active');
-  menu.classList.toggle('active');
+  if (hamburger) hamburger.classList.toggle('active');
+  if (menu) menu.classList.toggle('active');
 }
